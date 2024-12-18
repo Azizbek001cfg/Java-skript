2cae3ee809090ed43b8b38529db213608e232720
Azizbek <azizbek.aslonov2010@gmail.com>
2024-12-18 19:13:28 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,73 @@
+let name = "Akbar"
+let year = 2007
+let color = "red"
+let xtech = "oq"
+let viloyat = "Buhoro"
+
+
+console.log(`yosh ${2024-year}`);
+
+let a = 3
+let b = 5
+
+console.log(`Javob ${a-b}`);
+
+console.log(`qoshish ${a+b}`);
+console.log(`bolish ${a/b}`);
+console.log(`foiz ${a%b}`);
+console.log(`daraja ${a**b} tamom`);
+// console.log(`ayirish ${}`);
+// camel case
+
+// vazifa daftarga 1 kok 1 ta qizil ruchka kodladagi kerakli qismllani yoshish
+// qizil asosiya
+// 10ta ozgaruvchi 5ta sonlik 5ta satrlik 
+
+let ism = "Keldiyor"
+// ism => o'zgaruvchining nomi
+// "Keldiyor" => o'zgaruvchining qiymati
+
+// let yil = 2003
+// console.log(ism)
+// console.log(yosh)
+// console.log("samir", "sarvar", "javohir");
+// console.log((2024-yil) * 365);
+// console.log("Yosh =", 2024-yil, "da");
+// console.log(`Yosh = ${2024-yil} da`);
+
+// Qiymatni o'zgartirish:
+// let ustozYoshi = 21
+// ustozYoshi = 22
+// console.log(ustozYoshi);
+
+// Matematik amallar:
+// + => qo'shish
+// - => ayirish
+// * => ko'paytirish
+// / => bo'lish
+// % => qoldiq
+// ** => daraja
+// ++ => qiymatni bitta oshirish
+// -- => qiymatni bittaga kamaytirish
+
+let a = 3
+let b = 4
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+// b marta a ni o'ziga ko'paytirasan
+console.log(2**3);
+// 3 marta 2 ni o'ziga ko'paytirasan
+
+a++
+console.log(a);
+b--
+console.log(b);
+
+let p = 2*(a+b)
+console.log("Perimetr=", p);
+let s = a*b
+console.log("Yuza=", s);
